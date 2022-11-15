b81f8ce36a05a91837c95ad25195ef7cb6a597bd
Yaroslav <yaroslavkyzyk@gmail.com>
2022-11-15 21:31:29 +0200

    Balance Fractions
    - Made Demons pick only 1st warrior from collections;
    - Some numbers manipulations;

--- Assets/_Scriptables/Warrioir Pick Strategies/First In List Pick/FistInListPickStrategy.cs
+++ Assets/_Scriptables/Warrioir Pick Strategies/First In List Pick/FistInListPickStrategy.cs
@@ -9,9 +9,12 @@ namespace InfinityGame.Strategies.WarrioirPickStrategies
     [CreateAssetMenu(fileName = "FistInListPickStrategy", menuName = "Data/WarrioirChoseStrategies/FistInListPickStrategy", order = 52)]
     public class FistInListPickStrategy : WarrioirsPickStrategy
     {
+        [SerializeField] private int _countOfPicks = 1;
+
         public override IEnumerable<Warrior> ChoseWarrioirsToSpawn(IReadOnlyList<Warrior> warrioirs)
         {
-            yield return warrioirs[0];
+            for (int i = 0; i < _countOfPicks; i++)
+                yield return warrioirs[0];
         }
     }
 }
